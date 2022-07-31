e28b5da6a222490099a37bb569cdebe18ee82434
Tupi Guedes <tupiguedes@outlook.com>
2022-07-31 14:22:55 -0300

    Componente Leyout e Titulo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
+import Leyout from "../components/Leyout";
+
 export default function Home() {
   return (
     <div className={`
-      flex h-screen justify-center items-center 
-      bg-gradient-to-r from-purple-500 to-blue-600
+      flex justify-center items-center h-screen
+       bg-gradient-to-r from-blue-500 to-purple-500 
+       text-white
     `}>
-      <span className="text-4xf">Hello World</span>
+
+      <Leyout titulo="Cadastro Simples">
+        <span>Conteudo</span>
+      </Leyout>
     </div>
   )
 }
